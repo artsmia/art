7e19bdfccd31d56a270ecec9c712120c5639401e
Kjell <kjell@leanside.com>
2015-01-09 15:57:57 -0600

    testdrive react-router

---
+++ index.js
@@ -0,0 +1,56 @@
+var React = require('react'),
+  Router = require('react-router'),
+  { NotFoundRoute, Navigation, State, Link, Route, RouteHandler, DefaultRoute, Redirect } = Router,
+  resolveHash = require('when/keys').all,
+  rest = require('rest')
+
+var App = React.createClass({
+  render() {
+    return (
+      <RouteHandler {...this.props}/>
+    )
+  }
+})
+
+var Artwork = React.createClass({
+  mixins: [Router.State],
+  statics: {
+    fetchData: (params) => {
+      return rest('http://caption-search.dx.artsmia.org/id/'+params.id).then((r) => JSON.parse(r.entity))
+    }
+  },
+  render() {
+    return (
+      <div>
+        <h1>{this.props.data.artwork.title}</h1>
+        <h2>{this.props.data.artwork.artist}</h2>
+        <img src={`http://api.artsmia.org/images/${this.getParams().id}/small.jpg`} />
+      </div>
+    )
+  }
+})
+
+var routes = (
+  <Route handler={App} path="/">
+    <Route name="artwork" path="art/:id" handler={Artwork} />
+    <Redirect from="/" to="artwork" params={{id: 1425}} />
+  </Route>
+);
+
+Router.run(routes, (Handler, state) => {
+  window.Handler = Handler
+  window.state = state
+
+  var promises = state.routes.filter((route) => {
+    return route.handler.fetchData
+  }).reduce((promises, route) => {
+    promises[route.name] = route.handler.fetchData(state.params)
+    return promises
+  }, {})
+
+  resolveHash(promises).then(data => {
+    console.log('promises', promises, 'resolved to ', data)
+    React.render(<Handler data={data}/>, document.body)
+  })
+});
+
