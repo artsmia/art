04745d1210a475206d563350deac4e19d29ab50d
Kjell <kjell@leanside.com>
2015-03-03 17:27:31 -0600

    search/browse artists by first letter, name

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var Search = React.createClass({
       <div>
         <input type="search" placeholder="search for something" value={this.state.terms} onChange={this.throttledSearch} />
         <SearchResults {...this.props} updateInput={this.updateInput} />
+        <ArtistsByLetter {...this.props} />
       </div>
     )
   },
@@ -69,6 +70,37 @@ var SearchResults = React.createClass({
   }
 })
 
+var ArtistsByLetter = React.createClass({
+  mixins: [Router.State, Router.Navigation],
+
+  statics: { 
+    fetchData: (params) => {
+      return rest('http://cdn.dx.artsmia.org/artists.json').then((r) => {
+        return JSON.parse(r.entity).aggregations.artist.buckets
+      })
+    }
+  },
+
+  render() {
+    if(!this.props.data.artistsByName) return <div className="loading"></div>
+    var letter = this.getParams().letter
+    var letters = this.props.data.artistsByName.map((a) => a)
+    console.info(letters)
+    return (
+      <dl>
+      {letters.map((l) => {
+        return <div>
+          <dt key={l.key}><Link to="artistsByName" params={{letter: l.key}}>{l.key}</Link></dt>
+          {letter === l.key && <dd style={{position: 'absolute', top: '3em'}}>
+            {l.byName.buckets.map((b) => <Link to="searchResults" params={{terms: `artist.raw:%22${b.key}%22`}}>{b.key}</Link>)}
+          </dd>}
+        </div>
+      })}
+      </dl>
+    )
+  }
+})
+
 var Artwork = React.createClass({
   mixins: [Router.State],
   statics: {
@@ -93,6 +125,7 @@ var routes = (
     <Redirect from="/" to="search" />
     <Route name="search" path="/search/" handler={Search}>
       <Route name="searchResults" path="/search/:terms" handler={SearchResults} />
+      <Route name="artistsByName" path="/search/artists/:letter" handler={ArtistsByLetter} />
     </Route>
   </Route>
 );
