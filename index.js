3a60bc6f47ec87f8ec84dbb15a8a8c464ad1f7b5
Kjell <kjell@leanside.com>
2015-03-04 15:56:08 -0600

    list objects by id

--- index.js
+++ index.js
@@ -101,6 +101,25 @@ var ArtistsByLetter = React.createClass({
   }
 })
 
+var ObjectsById = React.createClass({
+  mixins: [Router.State],
+
+  statics: {
+    fetchData: (params) => rest(`http://caption-search.dx.artsmia.org/ids/${params.ids}`).then((r) => JSON.parse(r.entity))
+  },
+
+  render() {
+    var _self = this
+    var objects = this.props.data.objectsById.filter((o, index, objs) =>  o)
+    objects.forEach(o => o.id = o.id.replace('http://api.artsmia.org/objects/', ''))
+    return (
+      <div>
+        {objects.map((o) => <Artwork key={'object:'+o.id} id={o.id} data={{artwork: o}} />)}
+      </div>
+    )
+  }
+})
+
 var Artwork = React.createClass({
   mixins: [Router.State],
   statics: {
@@ -127,6 +146,7 @@ var routes = (
       <Route name="searchResults" path="/search/:terms" handler={SearchResults} />
       <Route name="artistsByName" path="/search/artists/:letter" handler={ArtistsByLetter} />
     </Route>
+    <Route name="objectsById" path="/search/ids/:ids" handler={ObjectsById} />
   </Route>
 );
 
