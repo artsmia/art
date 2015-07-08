336d309f07334fd5233511f5aac43e837e93144f
Kjell <kjell@leanside.com>
2015-07-08 13:31:22 -0500

    WIP: click to select does a 'peek' at facets
    The idea: when a facet is interesting, click on it to open a preview
    popup with more of that content. So on seeing 'Three Bathers', a click on
    'Henry Matisse' will activate a 'peek' for other art with `artist:"Henry
    Matisse"`.
    
    Or I want to see all the other stuff in G321 along with the thing I'm
    looking at. Click to select, open a popup, bam.
    
    From there I could continue on to that search, dive straight into an
    individual object, or close the peek and go about my business.

--- src/artwork-result.js
+++ src/artwork-result.js
@@ -1,6 +1,7 @@
 var React = require('react')
 var Router = require('react-router')
 var { Link } = Router
+var ClickToSelect = require('react-click-to-select')
 
 var ArtworkImage = require('./artwork-image')
 
@@ -22,9 +23,10 @@ var ArtworkResult = React.createClass({
     return (
       <div className='artwork-result'>
         <h1><span dangerouslySetInnerHTML={{__html: highlights && (highlights.title || highlights['title.ngram']) || art.title}}></span> ({id}, <a href={`https://collections.artsmia.org/index.php?page=detail&id=${id}`}>#</a>) <Link to="artwork" params={{id: id}}>&rarr;</Link></h1>
-        <h2><span dangerouslySetInnerHTML={{__html: highlights && (highlights.artist || highlights['artist.ngram']) || art.artist}}></span></h2>
+        <h2><ClickToSelect><span dangerouslySetInnerHTML={{__html: highlights && (highlights.artist || highlights['artist.ngram']) || art.artist}}></span></ClickToSelect></h2>
         <ArtworkImage art={art} id={id} />
-        <p className='location' >{art.room === 'Not on View' ? art.room : <strong>{art.room}</strong>}</p>
+        <p  >{art.room === 'Not on View' ? art.room : <strong>{art.room}</strong>}</p>
+        <p className='location'><ClickToSelect>{art.room === 'Not on View' ? art.room : <strong>{art.room}</strong>}</ClickToSelect></p>
         <div>
           {showHighlights.map((key) => {
             return <p key={`highlight${key}`} className={['highlight', key].join(' ')} dangerouslySetInnerHTML={{__html: highlights[key][0].replace('\n', '<br>')}}></p>
