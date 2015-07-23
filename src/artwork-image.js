c61d1a045561cac15b975b7784f1f7cdbbca30ae
niffur <jjurgens@artsmia.org>
2015-07-23 14:20:43 -0500

    yet more styles

--- src/artwork-image.js
+++ src/artwork-image.js
@@ -9,13 +9,13 @@ var ArtworkImage = React.createClass({
     let id = this.props.id
     let aspectRatio = art.image_height/art.image_width
     let maxWidth = Math.min(window.innerWidth, 400)
-    let height = aspectRatio > 1 ? 400 : aspectRatio*maxWidth
+    let height = '100%'
     let width = Math.ceil((1/aspectRatio)*height)
     let padding = width >= maxWidth ? -8 : -8+(maxWidth-width)/2
 
     return art.image == 'valid' && art.image_width > 0 && (
       <div className='artwork-image'>
-        <LazyLoad height={height+'px'}>
+        <LazyLoad height={height}>
           <img
             src={`http://api.artsmia.org/images/${id}/400/medium.jpg`}
             style={{maxWidth: maxWidth, margin: window.innerWidth <= 400 && `0 ${padding}`}} />
--- src/search-results.js
+++ src/search-results.js
@@ -28,7 +28,7 @@ var SearchResults = React.createClass({
     var search = this.props.data.searchResults
     var results = this.props.hits.map((hit) => {
       var id = hit._source.id.replace('http://api.artsmia.org/objects/', '')
-      return <div key={id}><ArtworkResult id={id} data={{artwork: hit._source}} highlights={hit.highlight} /><hr/></div>
+      return <div key={id}><ArtworkResult id={id} data={{artwork: hit._source}} highlights={hit.highlight} /></div>
     })
 
     return (
@@ -36,9 +36,15 @@ var SearchResults = React.createClass({
         <SearchSummary search={search} hits={this.props.hits} results={results} />
         <div className='objects-wrap' style={{clear: 'both'}}>{results}</div>
         <div className='objects-focus'>
-            <h2>The Death of Germanicus</h2>
+            <h2>The Death of Germanicus, <span className='date'>1627</span></h2>
             <h5>Nicholas Poussin</h5>
             <img src="http://placehold.it/800x800" />
+            <div className='tombstone'>
+                Oil on canvas<br />
+                58 1/4 x 78 in. (147.96 x 198.12 cm) (canvas)<br/>
+                The William Hood Dunwoody Fund 58.28
+            </div>
+            <p>The young Roman general Germanicus has just been poisoned by his jealous adoptive father, the emperor Tiberius. On his deathbed, Germanicus asks his friends to avenge his murder and his wife to endure her sorrow bravely. The subject of this, Poussin's first major history painting, comes from the Annales of the Roman historian Tacitus. The event occurred in 19 CE. A key work in Western painting, this tragic picture presents a moral lesson in stoic heroism, seen especially in the restraint and dignity of the mourning soldiers. This painting became the model for countless deathbed scenes for two centuries to come, particularly for Neoclassical art around 1800. Many powerful human themes figure here: death, suffering, injustice, grief, loyalty, and revenge. Poussin drew on Roman antiquity for the form as well as the subject of this painting. The composition, with its shallow spatial arrangement, is based on a Roman sarcophagus relief. Poussin spent most of his life in Rome, where he created a classical style that strongly influenced both French and Italian art.</p>
         </div>
       </div>
     )
