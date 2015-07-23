054bedf7d7ff04aa413e4e11cd46bc193dc179d0
Kjell <kjell@leanside.com>
2015-07-23 16:39:50 -0500

    'focus' search results on the right side

--- src/search-results.js
+++ src/search-results.js
@@ -5,6 +5,8 @@ var rest = require('rest')
 var SearchSummary = require('./search-summary')
 var ArtworkResult = require('./artwork-result')
 var SEARCH = require('./search-endpoint')
+var ArtworkImage = require('./artwork-image')
+var Markdown = require('./markdown')
 
 var SearchResults = React.createClass({
   mixins: [Router.State],
@@ -19,36 +21,55 @@ var SearchResults = React.createClass({
     }
   },
 
+  getInitialState() {
+    var firstHit = this.props.hits[0]
+    return {
+      focusedResult: firstHit && firstHit._source,
+    }
+  },
+
   shouldComponentUpdate(nextProps, nextState) {
     return this.props.data.searchResults != nextProps.data.searchResults ||
-      this.props.hits != nextProps.hits
+      this.props.hits != nextProps.hits ||
+      this.props.state != nextState
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.focusResult(nextProps.hits[0])
   },
 
   render() {
     var search = this.props.data.searchResults
     var results = this.props.hits.map((hit) => {
       var id = hit._source.id.replace('http://api.artsmia.org/objects/', '')
-      return <div key={id}><ArtworkResult id={id} data={{artwork: hit._source}} highlights={hit.highlight} /></div>
+      return <div key={id} onClick={this.focusResult.bind(this, hit)}>
+        <ArtworkResult id={id} data={{artwork: hit._source}} />
+      </div>
     })
+    var {focusedResult} = this.state
 
     return (
       <div className='search-results-wrap clearfix'>
         <SearchSummary search={search} hits={this.props.hits} results={results} />
         <div className='objects-wrap' style={{clear: 'both'}}>{results}</div>
-        <div className='objects-focus'>
-            <h2>The Death of Germanicus, <span className='date'>1627</span></h2>
-            <h5>Nicholas Poussin</h5>
-            <img src="http://placehold.it/800x800" />
+        {focusedResult && <div className='objects-focus'>
+            <h2>{focusedResult.title}, <span className='date'>{focusedResult.dated}</span></h2>
+            <h5>{focusedResult.artist}</h5>
+            <ArtworkImage art={focusedResult} id={focusedResult.id} />
             <div className='tombstone'>
-                Oil on canvas<br />
-                58 1/4 x 78 in. (147.96 x 198.12 cm) (canvas)<br/>
-                The William Hood Dunwoody Fund 58.28
+              {focusedResult.medium}<br />
+              {focusedResult.dimension}<br/>
+              {`${focusedResult.creditline} ${focusedResult.accession_number}`}
             </div>
-            <p>The young Roman general Germanicus has just been poisoned by his jealous adoptive father, the emperor Tiberius. On his deathbed, Germanicus asks his friends to avenge his murder and his wife to endure her sorrow bravely. The subject of this, Poussin's first major history painting, comes from the Annales of the Roman historian Tacitus. The event occurred in 19 CE. A key work in Western painting, this tragic picture presents a moral lesson in stoic heroism, seen especially in the restraint and dignity of the mourning soldiers. This painting became the model for countless deathbed scenes for two centuries to come, particularly for Neoclassical art around 1800. Many powerful human themes figure here: death, suffering, injustice, grief, loyalty, and revenge. Poussin drew on Roman antiquity for the form as well as the subject of this painting. The composition, with its shallow spatial arrangement, is based on a Roman sarcophagus relief. Poussin spent most of his life in Rome, where he created a classical style that strongly influenced both French and Italian art.</p>
-        </div>
+            <Markdown>{focusedResult.text}</Markdown>
+        </div>}
       </div>
     )
-  }
+  },
+
+  focusResult(hit) {
+    this.setState({focusedResult: hit._source})
+  },
 })
 
 module.exports = SearchResults
