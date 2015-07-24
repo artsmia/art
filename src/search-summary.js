f7c2cc91d76ed3f539451e8454d84a95d7e5d01c
Kjell <kjell@leanside.com>
2015-07-24 15:02:06 -0500

    hide aggregations by default, link to toggle

--- src/search-summary.js
+++ src/search-summary.js
@@ -4,17 +4,28 @@ var { Link } = require('react-router')
 var Aggregations = require('./aggregations')
 
 const SearchSummary = React.createClass({
+  getInitialState() {
+    return {
+      showAggs: false,
+    }
+  },
+
   render() {
     const search = this.props.search
     if(!search || !search.hits) return <div />
     const hits = this.props.hits
     const results = this.props.results
+    const {showAggs} = this.state
 
     const showAllLink = search &&
       <span>.&nbsp;(<Link to={search.filters ? 'filteredSearchResults' : 'searchResults'}
              params={{terms: search.query, splat: search.filters}}
              query={{size: search.hits.total}}>show all</Link>)
       </span>
+
+    const toggleAggs = hits.length > 0 && <span>
+      &nbsp; (<a onClick={this.toggleAggs}>{showAggs ? 'hide filters' : 'filter search'}</a>)
+    </span>
 
     const showingAll = hits.length == search.hits.total
     return (
@@ -25,11 +36,16 @@ const SearchSummary = React.createClass({
           results matching <code>{search.query}</code>
           {search.filters && <span> and <code>{search.filters}</code></span>}
           {showingAll || {showAllLink}}
+          {toggleAggs}
         </h2>
-        <Aggregations search={search} />
+        {showAggs && <Aggregations search={search} />}
       </div>
     )
-  }
+  },
+
+  toggleAggs() {
+    this.setState({showAggs: !this.state.showAggs})
+  },
 })
 
 module.exports = SearchSummary
