e825987ebad8e19789cd2474dc78df1f7c6d6c7a
Kjell <kjell@leanside.com>
2015-08-17 16:23:47 -0500

    Re-order filters, only 3 open by default
    Clicking on the title for the others toggles them

--- src/aggregations.js
+++ src/aggregations.js
@@ -16,16 +16,17 @@ var Aggregations = React.createClass({
     const search = this.props.search
     const {aggs} = this.state
     const customFilters = this.customFilters
+    const toggleAgg = (agg) => this.toggleAggregation.bind(this, agg)
 
     return (
       <div id="aggs" style={{width: '100%', overflowX: 'scroll', whiteSpace: 'nowrap', paddingBottom: '10px'}}>
         {aggs.map(function(agg) {
           const aggIsActive = search.filters && search.filters.match(new RegExp(agg.name, 'i'))
           // (search.filters.match(new RegExp(agg.name, 'i')) || customFilters[agg.name] && search.filters.match(new RegExp(customFilters[agg.name])
-          const showAgg = agg.buckets.length > 1 || aggIsActive
+          const showAgg = agg.buckets.length >= 1 || aggIsActive
           if(showAgg) return (<dl key={agg.name} id={agg.name} style={{display: 'inline-block', margin: '0', verticalAlign: 'top'}}>
-            <dt style={{fontWeight: aggIsActive && 'bold'}}>{agg.name.replace(/_/g, ' ')}</dt>
-            {agg.buckets.slice(0, 5).map(function(bucket) { 
+            <dt style={{fontWeight: aggIsActive && 'bold'}} onClick={toggleAgg(agg)}>{agg.name.replace(/_/g, ' ')}</dt>
+            {(agg.open || aggIsActive) && agg.buckets.slice(0, 5).map(function(bucket) { 
               const filterString = customFilters[agg.name] ? customFilters[agg.name][bucket.key] || bucket.key : agg.name.toLowerCase()+':"'+encodeURIComponent(bucket.key)+'"'
               const filterRegex = new RegExp(decodeURIComponent(filterString).replace(/([\[\]\?])/, '\\$1'), 'i')
               const bucketIsActive = search.filters && search.filters.match(filterRegex)
@@ -35,7 +36,7 @@ var Aggregations = React.createClass({
               const bucketText = `${bucket.key || '""'}`
 
               if(bucket.key) return (
-                <dd key={agg.name+bucket.key} style={{margin: '0', fontWeight: bucketIsActive && 'bold'}}><span className='mdl-badge' data-badge={bucket.doc_count}>
+                <dd key={agg.name+bucket.key} style={{margin: '0', fontWeight: bucketIsActive && 'bold', textDecoration: bucketIsActive && 'underline'}}><span className='mdl-badge' data-badge={bucket.doc_count}>
                   {<Link to={newFilters == '' ? 'searchResults' : 'filteredSearchResults'} params={{terms: `${search.query}`, splat: newFilters}}>
                       {bucketText}
                   </Link>}</span>
@@ -48,6 +49,13 @@ var Aggregations = React.createClass({
     )
   },
 
+  toggleAggregation(agg) {
+    var {aggs} = this.state
+    var open = aggs[aggs.indexOf(agg)].open
+    aggs[aggs.indexOf(agg)].open = !open
+    this.setState({aggs: aggs})
+  },
+
   // Aggregations come from ES as an object with named keys.
   //     {"On View": {…}, "Artist": {…}}
   // I want a `map`able array of the objects:
@@ -57,12 +65,13 @@ var Aggregations = React.createClass({
   buildAndSortAggregationsArray(props = this.props) {
     const aggs = props.search.aggregations
     const _aggs = []
-    const order = ["Artist", "Country", "On View", "Room", "Image", "Image_rights_type", "Department", "Title", "Style"]
+    const order = ["On View", "Image", "Department", "Artist", "Country", "Room", "Image_rights_type", "Title", "Style"]
 
     for(var agg in aggs) {
       _aggs.push({
         ...aggs[agg],
         name: agg,
+        open: order.slice(0, 3).indexOf(agg) > -1
       })
     }
 
