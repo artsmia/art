67f74a0915d2e1bec15c5b31458a6482ec9861a1
Kjell <kjell@leanside.com>
2015-08-26 17:08:55 -0500

    set height on lazyloaded images

--- src/artwork-image.js
+++ src/artwork-image.js
@@ -6,15 +6,15 @@ var Markdown = require('./markdown')
 var ArtworkImage = React.createClass({
   render() {
     let {art, id} = this.props
-    let aspectRatio = art.image_height/art.image_width
+    let aspectRatio = art.image_width/art.image_height
     let maxWidth = Math.min(window.innerWidth, 400)
-    let height = 'auto'
-    let width = Math.ceil((1/aspectRatio)*height)
+    let width = aspectRatio >= 1 ? maxWidth : maxWidth/aspectRatio
+    let height = aspectRatio >= 1 ? maxWidth/aspectRatio : maxWidth
     let padding = width >= maxWidth ? -8 : -8+(maxWidth-width)/2
 
     return art.image == 'valid' && art.image_width > 0 && (
       <div className='artwork-image'>
-        <LazyLoad>
+        <LazyLoad height={height}>
           <img
             src={`http://api.artsmia.org/images/${id}/400/medium.jpg`}
             style={{maxWidth: maxWidth, margin: window.innerWidth <= 400 && `0 ${padding}`}} />
