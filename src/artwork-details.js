833685da46b968c59ad6171deff05bb1a6881beb
niffur <jjurgens@artsmia.org>
2015-08-27 16:09:03 -0500

    Object Page styles+details added TMS info in the correct order except for Department since no Department info in API for an object record added typography

---
+++ src/artwork-details.js
@@ -0,0 +1,82 @@
+var React = require('react')
+
+var ArtworkDetails = React.createClass({
+
+  render() {
+    var art = this.props.art
+
+    return (
+        <div>
+          <h5 className='details-title'>Details</h5>
+          <div className='artwork-detail'>
+            <div className='detail-row'>
+                <div className='detail-title'>Title</div>
+                <div className='detail-content'>{art.title}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Dated</div>
+                <div className='detail-content'>{art.dated}</div>
+            </div>
+             <div className='detail-row'>
+                <div className='detail-title'>Artist</div>
+                <div className='detail-content'>{art.artist}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Nationality</div>
+                <div className='detail-content'>{art.nationality}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Role</div>
+                <div className='detail-content'>{art.role}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Gallery</div>
+                <div className='detail-content'>{art.room}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Description</div>
+                <div className='detail-content'>{art.description}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Dimension</div>
+                <div className='detail-content'>{art.dimension}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Credit</div>
+                <div className='detail-content'>{art.creditline}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Accession Number</div>
+                <div className='detail-content'>{art.accession_number}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Medium</div>
+                <div className='detail-content'>{art.medium}</div>
+            </div>
+            <div className='detail-row'>
+                <div className='detail-title'>Country</div>
+                <div className='detail-content'>{art.country}</div>
+            </div>
+             <div className='detail-row'>
+                <div className='detail-title'>Culture</div>
+                <div className='detail-content'>{art.culture}</div>
+            </div>
+             <div className='detail-row'>
+                <div className='detail-title'>Century</div>
+                <div className='detail-content'>{art.style}</div>
+            </div>
+             <div className='detail-row'>
+                <div className='detail-title'>Provenance</div>
+                <div className='detail-content'>{art.provenance}</div>
+            </div>
+             <div className='detail-row'>
+                <div className='detail-title'>Image Copyright</div>
+                <div className='detail-content'>{art.image_copyright}</div>
+            </div>
+          </div>
+        </div>
+    )
+  },
+})
+
+module.exports = ArtworkDetails
--- src/artwork-preview.js
+++ src/artwork-preview.js
@@ -19,20 +19,27 @@ var ArtworkPreview = React.createClass({
     var title = showLink ? 
       <Link to="artwork" params={{id: art.id}}>{art.title}</Link> :
       art.title
+      
+    /* if no artist, replace with culture */
 
     return (
       <div className='objects-focus' style={style}>
-        <h2>{title}, <span className='date'><Peek tag="span" showIcon={false}>{art.dated}</Peek></span></h2>
-        <h5><Peek facet="artist">{art.artist}</Peek></h5>
+        <h2>{title}</h2>
+        <h5 className='date'><Peek tag="span" showIcon={false}>{art.dated}</Peek></h5>
+        <h5><Peek facet='artist'>{art.artist}</Peek></h5>
+        <div className='display-bio'>
+            {art.nationality}, {art.life_date}<br/>
+            {art.role}
+        </div>
         <ArtworkImage art={art} id={art.id} />
-        <h6><Peek facet="room">{art.room}</Peek></h6>
+        <h6><Peek facet='room'>{art.room}</Peek></h6>
         <div className='tombstone'>
           <Peek facet="medium" tag="span">{art.medium}</Peek><br />
           {art.dimension}<br/>
           <Peek facet="creditline" tag="span">{art.creditline}</Peek>
           {art.accession_number}
         </div>
-        <Markdown>{art.text}</Markdown>
+        <Markdown>{art.text}</Markdown><br/>
         {showLink && <Link to="artwork" params={{id: art.id}}>View Details &rarr;</Link>}
       </div>
     )
