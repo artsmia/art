0e17ce50f7b6226898557bed5167102455501f73
niffur <jjurgens@artsmia.org>
2015-09-28 10:23:10 -0500

    initial peek styles

--- src/peek.js
+++ src/peek.js
@@ -51,7 +51,7 @@ var Peek = React.createClass({
       {this.state.open && this.state.facetedQ && <div className="peek" style={{fontSize: '80%', maxWidth: this.state.maxWidth || "100%"}}>
         {result && this.quiltFromResults()}
         <Link to="searchResults" params={{terms: this.state.facetedQ}}>
-          {result && result.hits && result.hits.total || 'search'} results for {this.state.query} {this.state.facet && `(${this.state.facet})`}
+          {result && result.hits && result.hits.total || 'search'} results for {this.state.query} {this.state.facet && `(${this.state.facet})`} <span className="more-results-link">View more results</span>
         </Link>
       </div>}
       {this.state.open && this.getQs().map((q) => <Peek facet={this.state.facet} q={q} key={q} />)}
