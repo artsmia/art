7ac6d9cfb8a7d2bbf2fc86f48189cb6f8d5b0cbe
Kjell <kjell@leanside.com>
2015-11-13 16:55:49 -0600

    shorter 'recent' explanation

--- src/decorate/recent.js
+++ src/decorate/recent.js
@@ -3,17 +3,9 @@ var React = require('react')
 var RecentDecorator =  React.createClass({
   render() {
     return <div className="decorator d-highlights"><p>
-      Mia's mission is to enrich the community by collecting, preserving, and
-      making accessible outstanding works of art from the world’s diverse
-      cultures. To accomplish this goal, Mia curators continually search for
-        important works of art to add to the collection. The objects are vetted
-      by the director and trustee committee before being formally added to the
-      museum’s inventory. These objects make Mia an exciting place of discovery
-      and learning about humanity’s diverse forms of expression. Each newly
-      accessioned object enriches the collection by offering greater
-      perspective on an artist, era, or culture. In the galleries, such objects
-      are typically identified by “Recent Acquisition” stickers on their
-      labels.
+      Mia continually searches for important works of art to add to the
+      collection and enrich it by offering greater perspective on an artist,
+      era, or culture.
     </p></div>
   }
 })
