8197204cb1b777a2d611df3b5294b0573e976589
Kjell <kjell@leanside.com>
2015-12-02 17:19:05 -0600

    fix most of the TMS -> markdown errors

--- src/markdown.js
+++ src/markdown.js
@@ -8,7 +8,10 @@ marked.setOptions({
 
 var Markdown = React.createClass({
   render() {
-    var content = this.props.children.replace('\n', '\n\n')
+    var content = this.props.children
+      .replace('\n', '\n\n')
+      .replace(/<i>([^<]+)<\/?i>/gi, '<i>$1</i>')
+
     if(!this.props.children) return <span />
     const rendered = this.props.alreadyRendered ? content : marked(content)
     return <div dangerouslySetInnerHTML={{__html: rendered}}></div>
