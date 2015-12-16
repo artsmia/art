d68f0d72300d116b10f6b1d4cb1a58a34d10e6d0
Kjell <kjell@leanside.com>
2015-12-16 12:08:41 -0600

    highlight active and hovered galleries

--- src/map.js
+++ src/map.js
@@ -19,7 +19,11 @@ var Map = React.createClass({
     </Isvg>
 
     return <div>
-      <span className={cx('map', {open: this.state.open})} onClick={this.handleClick}>
+      <span className={cx('map', {open: this.state.open})}
+        onClick={this.handleClick}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+      >
         {this.state.open ? svg : thumbnail}
       </span>
       {number && prevLink && <div>
@@ -37,12 +41,24 @@ var Map = React.createClass({
   },
 
   handleClick() {
-    console.info('handleClick', this.state.open)
     if(!this.state.open) return this.setState({open: true})
 
     var text = this.getSvgText(event.target)
     var gallery = text.textContent.match(/(\d+a?)/)[0]
+    this.highlightGallery(text)
     this.changeGallery(gallery)
+  },
+
+  handleMouseOver() {
+    if(!this.state.open) return // don't handle mouseover until map opens
+
+    var text = this.getSvgText(event.target)
+    var gallery = text.textContent.match(/(\d+a?)/)[0]
+    this.hoverGallery(text)
+  },
+
+  handleMouseOut() {
+    this.setState({hoveredPoly: null})
   },
 
   // recursively climb the SVG from where the click event happened
@@ -53,17 +69,55 @@ var Map = React.createClass({
 
     var parent = node.parentElement
     if(parent.nodeName !== 'g') return null
+
     var next = parent.querySelector(wantedNode) || parent
     return this.getSvgText(next)
   },
 
   handleSvgLoad(event) {
+    var allTexts = React.findDOMNode(this).querySelectorAll('svg text')
+    var activeText = Array.from(allTexts)
+    .filter(t => t.textContent == this.props.number)
+    this.highlightGallery(activeText && activeText[0])
   },
 
   changeGallery(nextGallery) {
     this.transitionTo('gallery', {gallery: nextGallery})
   },
 
+  highlightGallery(element) {
+    var poly = this.colorGallery(element)
+    var {activePoly} = this.state
+    this.setState({activePoly: poly})
+  },
+
+  hoverGallery(element) {
+    var poly = this.colorGallery(element)
+    var {hoveredPoly} = this.state
+    this.setState({hoveredPoly: poly})
+  },
+
+  colorGallery(element) {
+    var accentColor = '#fff'
+    var poly = element.parentElement.querySelector('polygon')
+    poly.style.setProperty('fill', accentColor)
+    return poly
+  },
+
+  resetGallery(poly) {
+    // `style.fill` overrides the `fill` attribute without clobbering it
+    // so we can reset it to the original color with `''`
+    poly.style.setProperty('fill', '')
+  },
+
+  componentDidUpdate(_, prevState) {
+    let prevActive = prevState.activePoly
+    let prevHovered = prevState.hoveredPoly
+    let {activePoly, hoveredPoly} = this.state
+
+    if(prevActive != activePoly) this.resetGallery(prevActive)
+    if(prevHovered != hoveredPoly && prevHovered != activePoly) this.resetGallery(prevHovered)
+  },
 })
 
 module.exports = Map
