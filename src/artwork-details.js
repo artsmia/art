ea4268073a8bdacf7489d52d8970c067b420b8a8
Kjell <kjell@leanside.com>
2016-01-23 13:31:42 -0600

    Add toggle-able extra content to art details
    where it makes sense

--- src/artwork-details.js
+++ src/artwork-details.js
@@ -1,8 +1,10 @@
 var React = require('react')
 var capitalize = require('capitalize')
 var Isvg = require('react-inlinesvg')
+var cx = require('classnames')
 
 var Markdown = require('./markdown')
+var Peek = require('./peek')
 
 var ArtworkDetails = React.createClass({
   build(field, fn) {
@@ -15,54 +17,97 @@ var ArtworkDetails = React.createClass({
 
     if(highlights && highlights[field]) value = <Markdown>{value[0]}</Markdown>
 
-    // merge `art` with `highlights`, replacing the un-highlighted values with
-    // their 'highlit' equivalent
-    var artAndHighlights = !highlights ? art : Object.assign({...art}, Object.keys(highlights).reduce((object, key) => {
+    var artAndHighlights = this.artAndHighlights()
+    var [content, extraContent] = fn ? fn(artAndHighlights, art) : [artAndHighlights[field]]
+    var showExtra = extraContent && this.state.expandDetailsMatrix[field]
+    var classes = cx("detail-row", {expandable: extraContent})
+
+    return <div className={classes} onClick={this.toggleExtra.bind(this, field)}>
+      <dt className="detail-title">{humanFieldName}</dt>
+      <dd className='detail-content'>{content}</dd>
+      {showExtra && <div className="detail-extra">{extraContent}</div>}
+    </div>
+  },
+
+  toggleExtra(field) {
+    var {expandDetailsMatrix} = this.state
+    expandDetailsMatrix[field] = !expandDetailsMatrix[field]
+    this.setState({expandDetailsMatrix})
+  },
+
+  // merge `art` with `highlights`, replacing the un-highlighted values with
+  // their 'highlit' equivalent
+  artAndHighlights() {
+    var {art, highlights} = this.props
+
+    this.merged = this.merged || !highlights ? art : Object.assign({...art}, Object.keys(highlights).reduce((object, key) => {
       var value = highlights[key] && highlights[key][0]
       if(typeof value == 'string') value = <Markdown>{value}</Markdown>
       object[key] = value
       return object
     }, {}))
 
-    var [content, extraContent] = fn ? fn(artAndHighlights, art) : [artAndHighlights[field]]
+    return this.merged
+  },
 
-    return <div className="detail-row">
-      <dt className="detail-title">{humanFieldName}</dt>
-      <dd className='detail-content'>{content}</dd>
-      {extraContent && <div className="detail-extra">{extraContent}</div>}
-    </div>
+  buildPeekableDetail(field) {
+    var {art} = this.props
+    var highlights = this.artAndHighlights()
+
+    return [
+      field,
+      art => [
+        highlights[field],
+        <Peek facet={field} q={art[field]} />
+      ]
+    ]
+  },
+
+  details() {
+    return [
+      ['title'],
+      this.buildPeekableDetail('dated'),
+      this.buildPeekableDetail('artist'),
+      this.buildPeekableDetail('nationality'),
+      ['role'],
+      ['gallery', (art, raw) => [art.room, <Peek facet="room" q={raw.room} />]],
+      this.buildPeekableDetail('department'),
+      ['dimension', (art, rawArt) => [art.dimension, art.dimension && <div>
+        {rawArt.dimension.match(/x.*cm/) && <Isvg src={`http://localhost:4009/svgs/${art.id}/dimension.svg`} />}
+      </div>]],
+      ['credit', (art, raw) => [art.creditline, <Peek facet="creditline" q={raw.creditline} />]],
+      ['accession_number'],
+      this.buildPeekableDetail('medium'),
+      this.buildPeekableDetail('country'),
+      this.buildPeekableDetail('culture'),
+      ['century', (art, raw) => [art.style, <Peek facet="style" q={raw.style} />]],
+      ['provenance'],
+      ['rights', (art, raw) => {
+        return [(art.image_copyright || art.rights_type) && <div>
+          {art.image_copyright && decodeURIComponent(art.image_copyright)}
+          {art.image_copyright && art.rights_type && <br/>}
+          {art.rights_type && <span>{art.rights_type}</span>}
+        </div>,
+        <Peek facet="rights_type" q={raw.rights_type} />
+        ]
+      }],
+      ['marks'],
+    ]
+  },
+
+  getInitialState() {
+    return {
+      expandDetailsMatrix: this.details().reduce((reduced, [field]) => {
+        reduced[field] = field == 'dimension'
+        return reduced
+      }, {})
+    }
   },
 
   render() {
     var {art, highlights} = this.props
     var skip = this.props.skipFields ? this.props.skipFields.split(' ') : []
-    var details = [
-      ['title'],
-      ['dated'],
-      ['artist'],
-      ['nationality'],
-      ['role'],
-      ['gallery', art => art.room],
-      ['department'],
-      ['dimension', (art, rawArt) => [art.dimension, art.dimension && <div>
-        {rawArt.dimension.match(/x.*cm/) && <Isvg src={`http://localhost:4009/svgs/${art.id}/dimension.svg`} />}
-      </div>]],
-      ['credit', art => art.creditline],
-      ['accession_number'],
-      ['medium'],
-      ['country'],
-      ['culture'],
-      ['century', art => art.style],
-      ['provenance'],
-      ['rights', art => {
-        return <div>
-          <span>{decodeURIComponent(art.image_copyright)}</span>
-          {art.image_copyright && art.image_rights_type && <br/>}
-          {art.image_rights_type && <span>{art.image_rights_type}</span>}
-        </div>
-      }],
-      ['marks'],
-    ]
+    var details = this.details()
     if(highlights && highlights.description) details.push(['description'])
     details = details.filter(([field]) => skip.indexOf(field) < 0)
     .map(field => this.build(...field))
