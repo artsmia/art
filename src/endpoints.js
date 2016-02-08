3e2d47bb2b4bbf854c6524f320c6e70e98ff4fee
Kjell <kjell@leanside.com>
2016-02-08 14:25:37 -0600

    a real search URL!

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,5 @@
 module.exports = {
-  search: "http://collections.artsmia.org:4680",
+  search: "http://search.artsmia.org",
   info: "http://artsmia.github.io/collection-info/index.json",
   dimensionSvg: (id, file) => `http://collections.artsmia.org/dimensions/${id}/${file || 'dimensions'}.svg`,
 }
