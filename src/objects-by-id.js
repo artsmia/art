5abd5b13dc8128027dadb3e02d346aa12db9de62
Kjell <kjell@leanside.com>
2016-02-22 15:17:33 -0600

    use Search to show objects by id

--- src/objects-by-id.js
+++ src/objects-by-id.js
@@ -4,23 +4,19 @@ var rest = require('rest')
 
 var SEARCH = require('./endpoints').search
 var ArtworkResult = require('./artwork-result')
+var Search = require('./search')
 
 var ObjectsById = React.createClass({
   mixins: [Router.State],
 
   statics: {
-    fetchData: (params) => rest(`${SEARCH}/ids/${params.ids}`).then((r) => JSON.parse(r.entity))
+    fetchData: {
+      searchResults: (params) => rest(`${SEARCH}/ids/${params.ids}`).then((r) => JSON.parse(r.entity)),
+    },
   },
 
   render() {
-    var _self = this
-    var objects = this.props.data.objectsById.filter((o, index, objs) =>  o)
-    objects.forEach(o => o.id = o.id.replace('http://api.artsmia.org/objects/', ''))
-    return (
-      <div className='objects-by-id'>
-        {objects.map((o) => <ArtworkResult key={'object:'+o.id} id={o.id} data={{artwork: o}} />)}
-      </div>
-    )
+    return <Search {...this.props} />
   }
 })
 
