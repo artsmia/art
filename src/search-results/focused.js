492cc963a1b706ac24ce076029e3758ffd856972
Kjell <kjell@leanside.com>
2016-03-18 11:07:07 -0500

    always show related content in preview
    vs only showing it when it matched a highlight

--- src/search-results/focused.js
+++ src/search-results/focused.js
@@ -10,7 +10,6 @@ var FocusedResult = React.createClass({
     var leftSpacing = this.props.leftColumnWidth.replace(/(\d+)/, int => parseInt(int)+3)
     var {art, highlights} = this.props
     var hasRelatedHighlight = highlights && Object.keys(highlights).join(' ').match(/related:/)
-    var showRelated = this.props.showRelated || hasRelatedHighlight
 
     return <div className="focusedResult" style={{position: 'absolute', top: 0, left: leftSpacing, right: 0, bottom: 0, width: '60%'}}>
       <Sticky stickyStyle={{right: 0, top: 0, position: 'fixed', left: leftSpacing, width: '60%'}}>
@@ -18,9 +17,7 @@ var FocusedResult = React.createClass({
         <span style={{position: 'absolute', right: '1em', marginTop: '1em', cursor: 'pointer'}} onClick={this.close}>
           <i className="material-icons">clear</i>
         </span>
-        {showRelated &&
-          <ArtworkRelated id={art.id} art={art} skipWrapper={true} highlights={highlights} />
-        }
+        <ArtworkRelated id={art.id} art={art} skipWrapper={true} highlights={highlights} />
       </Sticky>
     </div>
   },
