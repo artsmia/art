6814cc6e18ede8578faeb5465547b5d5d2885529
Kjell <kjell@leanside.com>
2016-06-22 16:21:35 -0500

    load collection info from outside the museum
    route around a network connectivity issue

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,6 +1,6 @@
 module.exports = {
   search: "http://search.artsmia.org",
-  info: "http://cdn.dx.artsmia.org/collection-info/index.json",
+  info: "http://collections.artsmia.org/_info/index.json",
   dimensionSvg: (id, file) => `http://collections.artsmia.org/dimensions/${id}/${file || 'dimensions'}.svg`,
   relatedInfo: (id) => `http://collections.artsmia.org/links/${id}.json`,
   galleryPanel: (room) => `https://cdn.rawgit.com/artsmia/mia-gallery-panels/master/${room}.md`,
