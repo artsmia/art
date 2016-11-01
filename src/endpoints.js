c5f4bd76bd108ec1bd3a100dd1cb418dc93c027e
Kjell <kjell@leanside.com>
2016-11-01 14:24:15 -0500

    pull dimension images from S3

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,7 @@
 module.exports = {
   search: "http://search.artsmia.org",
   info: "http://collections.artsmia.org/_info/index.json",
-  dimensionSvg: (id, file) => `http://collections.artsmia.org/dimensions/${id}/${file || 'dimensions'}.svg`,
+  dimensionSvg: (id, file) => `http://mia-dimensions.s3.amazonaws.com/${id}/${file || 'dimensions'}.svg`,
   relatedInfo: (id) => `http://collections.artsmia.org/links/${id}.json`,
   galleryPanel: (room) => `https://cdn.rawgit.com/artsmia/mia-gallery-panels/master/${room}.md`,
 }
