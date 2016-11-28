2459d3ae6a4cebd1d36c15e639fe1ff78a994337
emhavens <misty.havens@gmail.com>
2016-11-28 15:31:58 -0600

    added a bit of differentiation to the accession highlight text.

--- src/accession-highlight.js
+++ src/accession-highlight.js
@@ -25,7 +25,8 @@ var AccessionHighlight = React.createClass({
     return <div>
       <Artwork {...this.props} accessionHighlightView={true}>
         <ArtworkPreview art={art} showDuplicateDetails={true}>
-          <div className="description" itemProp="description">
+          <div className="description" itemProp="description" style={{'borderTop':'4px solid #232323', 'borderBottom':'4px solid #232323', 'padding':'4px 0', 'margin':'10px 0'}}>
+            <h2>Accession Highlight Overview</h2>
             <Markdown>{art.accessionHighlightText}</Markdown>
           </div>
         </ArtworkPreview>
