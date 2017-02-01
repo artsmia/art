9f7fb73dc0b1ee46479affadcc016b7fa23e425c
Kjell <kjell@leanside.com>
2017-02-01 10:41:54 -0600

    work around node+ssl incompatibility on /curators

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,8 @@
+var protocol = (typeof(window) == "undefined" ? "http" : "https")+"://"
 module.exports = {
-  search: (typeof(window) == "undefined" ? "http" : "https") + "://search.artsmia.org",
-  info: "https://collections.artsmia.org/_info/index.json",
+  search: protocol+"search.artsmia.org",
+  info: protocol+"collections.artsmia.org/_info/index.json",
   dimensionSvg: (id, file) => `https://mia-dimensions.s3.amazonaws.com/${id}/${file || 'dimensions'}.svg`,
   galleryPanel: (room) => `https://cdn.rawgit.com/artsmia/mia-gallery-panels/master/${room}.md`,
-  exhibition: (id) => `https://cdn.dx.artsmia.org/exhibitions/${Math.floor(id/1000)}/${id}.json`,
+  exhibition: (id) => protocol+`cdn.dx.artsmia.org/exhibitions/${Math.floor(id/1000)}/${id}.json`,
 }
