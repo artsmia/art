c20fc088aeb714fe823c34879977a8302d74495a
Kjell <kjell@leanside.com>
2017-02-01 16:15:12 -0600

    fix highlighting bug
    a highlighted field such as `exhibition.title` could mistakenly replace
    `title` due to how this was identifying potential highlighted matches.
    Oops!

--- src/highlighter.js
+++ src/highlighter.js
@@ -2,7 +2,7 @@ function highlightField(source, highlights, field) {
   if(!highlights) return source[field]
 
   var possibleHighlights = Object.keys(highlights)
-  .filter(key => key.match(field))
+  .filter(key => key.match(new RegExp("^"+field)))
   .map(key => highlights[key][0])
 
   return highlights && possibleHighlights && possibleHighlights[0]
