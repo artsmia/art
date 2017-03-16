15d69ba5fc46c48dec787b70e57f5fa0ad08f878
Kjell <kjell@leanside.com>
2017-03-16 15:33:51 -0500

    develop random artwork voting booth
    fun stuff: HOC for grabbing a random artwork and passing it down as
    props to a composed component

---
+++ src/random-artwork.js
@@ -0,0 +1,39 @@
+var React = require('react')
+var rest = require('rest')
+
+var ArtworkPreview = require('./artwork-preview')
+var SEARCH = require('./endpoints').search
+
+var RandomArtworkContainer = (ComposedComponent, searchQuery) => React.createClass({
+  getInitialState() {
+    return {
+      art: null,
+    }
+  },
+
+  componentDidMount() {
+    var id = this.props.initialId
+
+    this.setArtwork(id)
+  },
+
+  setArtwork(id) {
+    var searchUrl = id ? `${SEARCH}/id/${id}` : `${SEARCH}/random/art`
+    if(!id && searchQuery) searchUrl += `?q=${searchQuery}`
+    console.info(searchUrl)
+    rest(searchUrl)
+    .then(data => this.setState({art: JSON.parse(data.entity)}))
+  },
+
+  changeArtwork() {
+    this.setArtwork()
+  },
+
+  render() {
+    return this.state.art ?
+      <ComposedComponent art={this.state.art} changeArtwork={this.changeArtwork} /> :
+      <span />
+  },
+})
+
+module.exports = RandomArtworkContainer
--- src/survey.js
+++ src/survey.js
@@ -3,8 +3,45 @@ var ReactDOM = require('react-dom')
 var Router = require('react-router')
 var rest = require('rest')
 
+var RandomArtwork = require('./random-artwork')
+var ArtworkPreview = require('./artwork-preview')
+
+// Example:
+//
+// var ArtworkCard = (props) => {
+//   return <div>
+//     <ArtworkPreview {...props}>
+//       <a href="#" onClick={props.changeArtwork} value="Next!">Next!</a>
+//     </ArtworkPreview>
+//   </div>
+// }
+
+// var RandomArtworkCard = RandomArtwork(ArtworkCard, 'room:G3* image:valid public_access:1')
+
+var VotingBooth = React.createClass({
+  render() {
+    var linkStyle = {margin: '1em', display: 'inline-block'}
+    return <div>
+      <ArtworkPreview {...this.props}>
+        <a href="#" style={linkStyle} onClick={this.vote.bind(this, 1)}>&uarr; I like this</a>
+        <a href="#" style={linkStyle} onClick={this.vote.bind(this, -1)}>&darr; I don't like this</a>
+      </ArtworkPreview>
+    </div>
+  },
+
+  vote(direction) {
+    console.info('voting', direction)
+    this.props.changeArtwork()
+  },
+})
+
+var VoteForRandomArtworkCard = RandomArtwork(VotingBooth, 'room:G3* image:valid public_access:1')
+
 var Survey = React.createClass({
   render() {
+    return <div style={{margin: '3em'}}>
+      <VoteForRandomArtworkCard />
+    </div>
   },
 })
 
