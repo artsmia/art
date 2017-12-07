35b402ee0662dff415414aded8e81cd0ac2de775
Kjell <kjell@leanside.com>
2017-12-07 14:30:49 -0600

    pull collection-info from github pages built JSON
    At some point jekyll on github pages wouldn't build
    `artsmia/collection-info`, but it does now. Pulling the info directly
    from GH pages removes the need to hand-build and deploy the JSON file

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,7 @@
 var protocol = (typeof(window) == "undefined" ? "http" : "https")+"://"
 module.exports = {
   search: protocol+"search.artsmia.org",
-  info: protocol+"collections.artsmia.org/_info/index.json",
+  info: "https://artsmia.github.io/collection-info/index.json",
   dimensionSvg: (id, file) => `https://mia-dimensions.s3.amazonaws.com/${id}/${file || 'dimensions'}.svg`,
   galleryPanel: (room) => `https://cdn.rawgit.com/artsmia/mia-gallery-panels/master/${room}.md`,
   exhibition: (id) => protocol+`cdn.dx.artsmia.org/exhibitions/${Math.floor(id/1000)}/${id}.json`,
