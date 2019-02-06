8207548cbcd700b4a16ab9ddea844e3cbc553f60
Kjell <kjell@leanside.com>
2019-02-06 13:20:15 -0600

    filter out unfound ids when searching by id

--- src/objects-by-id.js
+++ src/objects-by-id.js
@@ -11,7 +11,26 @@ var ObjectsById = React.createClass({
 
   statics: {
     fetchData: {
-      searchResults: (params) => rest(`${SEARCH}/ids/${params.ids}`).then((r) => JSON.parse(r.entity)),
+      searchResults: (params) => rest(`${SEARCH}/ids/${params.ids}`)
+      .then((r) => JSON.parse(r.entity))
+      .then(json => {
+        // remove any ids that weren't found in ES
+        const existingHits =  json.hits.hits.filter(hit => {
+          const hasData = hit && hit.found
+          if(!hasData) console.error('no data for', hit)
+          return hasData
+        })
+
+        // and reshape the ES payload to look the same, but exclude them
+        return {
+          ...json,
+          hits: {
+            ...json.hits,
+            total: existingHits ? existingHits.length : 0,
+            hits: existingHits,
+          }
+        }
+      }),
     },
   },
 
