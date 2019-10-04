e954fd555f44b0c00542e02cc555bb2842bb82e6
Kjell <kjell@leanside.com>
2019-10-04 16:01:20 -0500

    update homepage language and endpoints
    jsdelivr because rawgit closed up shop

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,8 +1,9 @@
 var protocol = (typeof(window) == "undefined" ? "http" : "https")+"://"
 module.exports = {
   search: protocol+"search.artsmia.org",
+  // search: 'http://localhost:4680',
   info: "https://artsmia.github.io/collection-info/index.json",
   dimensionSvg: (id, file) => `https://mia-dimensions.s3.amazonaws.com/${id}/${file || 'dimensions'}.svg`,
-  galleryPanel: (room) => `https://cdn.rawgit.com/artsmia/mia-gallery-panels/master/${room}.md`,
+  galleryPanel: (room) => `"https://cdn.jsdelivr.net/gh/artsmia/mia-gallery-panels@master/${room}.md`,
   exhibition: (id) => protocol+`cdn.dx.artsmia.org/exhibitions/${Math.floor(id/1000)}/${id}.json`,
 }
