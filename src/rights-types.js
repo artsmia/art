ca0312476320f7cbdc41c901d413a2290a817327
nick <nick.laffey@gmail.com>
2019-12-27 14:54:39 -0500

    Temporarily setting rightsstatements to an empty array

--- src/rights-types.js
+++ src/rights-types.js
@@ -11,15 +11,21 @@ const publicDomainMark = {
   definition: 'This work has been identified as being free of known restrictions under copyright law, including all related and neighboring rights.\n\nYou can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission. See Other Information below.',
 }
 
+// TODO: It appears that this module is installed locally. For now I'm just going to comment this out and replace it
+// with an empty array so I can get to the accessibility fixes.
+// const importedRightsStatements = require('rightsstatements');
+
+const importedRightsStatements = [];
+
 const rightsstatements = [
-  ...require('rightsstatements').map(rs => {
+  ...importedRightsStatements.map(rs => {
     rs.definition = rs.definition
       .replace('The copyright and related rights status of this Item has not been evaluated', 'The copyright and related rights status of this Item has not been evaluated by the Minneapolis Institute of Art (Mia)')
       .replace('Please refer to the organization that has made the Item available for more information.', 'Please contact us for more information.')
       .replace(/The organization that has made the Item available/i, 'Minneapolis Institute of Art (Mia)') // TODO link 'contact us' to collectionsdata@artsmia.org
       .replace('the organization was', 'we were')
 
-    if(rs.definition.match(/The organization/)) {
+    if (rs.definition.match(/The organization/)) {
       console.info(rs.label, rs.definition, rs.identifier)
       // debugger
     }
@@ -46,7 +52,7 @@ const legacyToStandardizedStmt = {
   "Full Permission": "In Copyright - Non-Commercial Use Permitted",
   "Orphaned Work": "In Copyright - Rights-holder(s) Unlocatable or Unidentifiable",
   "Permission Denied": "In Copyright", // TODO what goes here? Empty in Heidi's mapping
-  "Public Domain": "Public Domain", 
+  "Public Domain": "Public Domain",
   "No Known Copyright Restrictions": "No Known Copyright",
   "Need Permission": "Copyright Not Evaluated",
 }
@@ -56,8 +62,8 @@ const _rightsStatements = Object.keys(miaLegacyRightsTypes).map(legacyName => {
 
   const legacyToStand = legacyToStandardizedStmt[legacyName]
   const rightsStmt = rightsstatements.find(stmt => stmt.label === legacyToStand)
-  if(!rightsStmt && legacyName !== 'getRights') {
-    console.info({legacyName, legacyToStand, rightsStmt})
+  if (!rightsStmt && legacyName !== 'getRights') {
+    console.info({ legacyName, legacyToStand, rightsStmt })
   }
 
   return {
@@ -72,11 +78,11 @@ const _rightsStatements = Object.keys(miaLegacyRightsTypes).map(legacyName => {
 // needs to use `rights_type`. Some artworks only have `rights`, some have both.
 //
 // TODO reindex all of ES to remove `rights` and  populate `rights_type`
-const getRights = function(art) {
+const getRights = function (art) {
   art.rights_type = art.rights_type || art.rights
   art.rights = false
   return art.rights_type
-}
+};
 
 // Due to limitations in our TMS database, we don't exactly map rights statements correctly.
 // This is a map that corrects our shortened statements (…due to a character limit on the field in SQL…)
@@ -93,20 +99,20 @@ const miaRightsLabelToRightsStatementID = {
 // this would have been 10x easier if we'd just used the short hand identifier,
 // but probably wouldn't read well in TMS
 
-function RightsStatementIcon({statement, color, style, ...props}) {
-  if(!statement) return <span />
+function RightsStatementIcon({ statement, color, style, ...props }) {
+  if (!statement) return <span />
 
   var shortId = statement.identifier.split('-')[0]
-  var iconURL = statement.identifier === 'CC-PDM' ? 
+  var iconURL = statement.identifier === 'CC-PDM' ?
     `https://upload.wikimedia.org/wikipedia/commons/a/af/Cc-public_domain_mark.svg`
     : `https://rightsstatements.org/files/icons/${shortId}.Icon-Only.${color || 'white'}.svg`
-  var fullIconURL = statement.identifier === 'CC-PDM' ? 
+  var fullIconURL = statement.identifier === 'CC-PDM' ?
     `https://upload.wikimedia.org/wikipedia/commons/a/af/Cc-public_domain_mark.svg`
     : `https://rightsstatements.org/files/buttons/${statement.identifier}.${color || 'white'}.svg`
 
   return <img
     src={fullIconURL}
-    style={{paddingRight: '0.43em', display: 'inline', height: '1em', ...style}}
+    style={{ paddingRight: '0.43em', display: 'inline', height: '1em', ...style }}
     alt={`${statement.label} Rights Statement icon`}
     alt=""
     {...props}
