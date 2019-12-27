70ae3a5145b1b479b04af2355bd3665bcafe6573
nick <nick.laffey@gmail.com>
2019-12-27 14:55:05 -0500

    Creating SkipToMainContentLink component

---
+++ src/skip-to-main-content-link.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var SkipToMainContentLink = React.createClass({
+  render() {
+    return <a href="#main" className="skip-main">Skip to main content</a>
+  },
+});
+
+module.exports = SkipToMainContentLink;
+
