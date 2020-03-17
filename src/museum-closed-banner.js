493f0f38552e5304721d82b66943515797a90e59
Kjell <kjell@leanside.com>
2020-03-17 16:33:17 -0500

    update banner language

--- src/museum-closed-banner.js
+++ src/museum-closed-banner.js
@@ -1,7 +1,7 @@
 var React = require('react')
 
 module.exports = (props) => <p style={{backgroundColor: '#ef3535', color: 'white', textAlign: 'center', padding: '1em'}}>
-  In consideration of the health and safety of visitors, volunteers, and staff, Mia will temporarily close to the public starting Friday, March 13.
-  {' '}<a href="https://new.artsmia.org/covid-19" style={{color: 'white'}}>Learn more here.</a>
+  In consideration of the health and safety of visitors, volunteers, and staff, Mia is temporarily closed to the public.
+  {' '}<a href="https://new.artsmia.org/covid-19" style={{color: 'white', textDecoration: 'underline'}}>Learn more here.</a>
 </p>
 
