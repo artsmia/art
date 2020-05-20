3d8ed89163cc29ce23b5292592eb4a659ed1298f
Kjell <kjell@leanside.com>
2020-05-20 11:34:37 -0500

    swap `react-ga` for static google tag manager

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var Router = require('react-router')
 var fetchComponentData = require('./fetch')
-var ga = require('react-ga')
 
 var routes = require('./routes')
-var trackAnalytics = process.env.NODE_ENV == 'production'
-
-if(trackAnalytics) ga.initialize(process.env.GA_TRACKING_ID)
 
 window.history && history.replaceState({}, '', window.location.href.replace(/\/(.*)\/$/, '/$1'))
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
-  if(trackAnalytics) ga.pageview(state.pathname)
   window.privilegedClientIP = true
 
   var rehydratedData = window.__DATA__
