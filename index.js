83921576980351ce829d33f0ff97531d8d8fa390
Kjell <kjell@leanside.com>
2020-07-08 19:21:03 -0500

    add fathom analytics

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var Router = require('react-router')
 var fetchComponentData = require('./fetch')
+var fathom = require('fathom-client')
 
 var routes = require('./routes')
+
+var trackAnalytics = process.env.NODE_END === 'production'
+
+if(trackAnalytics) Fathom.load('PXCWTLRI', {
+  includedDomains: ['collections.artsmia.org']
+})
 
 window.history && history.replaceState({}, '', window.location.href.replace(/\/(.*)\/$/, '/$1'))
 
 Router.run(routes, Router.HistoryLocation, (Handler, state) => {
+  if(trackAnalytics) Fathom.trackPageview()
+  debugger
   window.privilegedClientIP = true
 
   var rehydratedData = window.__DATA__
