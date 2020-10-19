3e914ea550b8d19ef54d66f64e3ec3a65e60d7de
Kjell <kjell@leanside.com>
2020-10-19 15:27:29 -0500

    smooth out image grid
    Implement it as a Reakit <Grid> component for accessibility
    based on browser width, split all images into rows
    adjust flexbox styling so rows fill the width and adjust height by `auto`

--- foot-in-the-door/components/RoomGrid.js
+++ foot-in-the-door/components/RoomGrid.js
@@ -1,10 +1,19 @@
 /** @format */
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import {
+  unstable_useGridState as useGridState,
+  unstable_Grid as Grid,
+  unstable_GridRow as GridRow,
+  unstable_GridCell as GridCell,
+} from 'reakit/Grid'
 
 import { getImageSrc } from '../util'
 
 function RoomGrid(props) {
   const { hits, focused, perPage, classification, ...containerProps } = props
+
+  const grid = useGridState()
 
   const artworks = hits
     // When there is a 'focused' artwork, remove it from the grid so it
@@ -12,39 +21,101 @@ function RoomGrid(props) {
     .filter((art) => art !== focused)
     .slice(0, perPage)
 
+  // Columns as a function of window width.
+  // This should be a log function or clamped somehow?
+  // Ideally, small screens get 2 columns, and big screens get a max of 5?
+  // default to 2 for server rendered pages for mobile first
+  const { width: windowWidth } = useWindowSize()
+  const gridCols = windowWidth
+    ? Math.min(5, Math.max(2, Math.floor(windowWidth / 234)))
+    : 2
+
   return (
     <section {...containerProps}>
       <p className="uppercase text-center mb-8 font-hairline">
         Scroll to enter <strong>{classification}</strong> wing
       </p>
-      <div className="flex flex-wrap">
-        {artworks.map((art) => {
-          const {
-            _source: source,
-            _source: { id, title, artist, description },
-          } = art
-
+      <Grid {...grid} aria-label="Search Results" className="flex flex-wrap">
+        {chunkArray(artworks, gridCols).map((row, rowIndex) => {
           return (
-            <figure className="group flex-grow" key={id}>
-              <Link href={`/art/${id}`}>
-                <a>
-                  <img
-                    src={getImageSrc(source)}
-                    className="h-64 p-1"
-                    alt={description}
-                  />
-                  <figcaption className="hidden group-hover:block max-w-full">
-                    <h2>{title}</h2>
-                    <h3>{artist}</h3>
-                  </figcaption>
-                </a>
-              </Link>
-            </figure>
+            <GridRow {...grid} key={rowIndex} className="flex min-h-64 my-4">
+              {row.map((art) => {
+                const {
+                  _source: source,
+                  _source: { id, title, artist, description },
+                } = art
+
+                return (
+                  <GridCell
+                    {...grid}
+                    key={id}
+                    as="figure"
+                    className="group flex-grow"
+                  >
+                    <Link href={`/art/${id}`}>
+                      <a className="relative">
+                        <img
+                          src={getImageSrc(source)}
+                          className="h-auto w-full p-1"
+                          alt={description}
+                        />
+                        <figcaption className="hidden group-hover:block max-w-full">
+                          <h2>{title}</h2>
+                          <h3>{artist}</h3>
+                        </figcaption>
+                      </a>
+                    </Link>
+                  </GridCell>
+                )
+              })}
+            </GridRow>
           )
         })}
-      </div>
+      </Grid>
     </section>
   )
 }
 
 export default RoomGrid
+
+function chunkArray(items, size = 3) {
+  let chunks = [items.splice(0, size)]
+
+  while (items.length) {
+    chunks.push(items.splice(0, size))
+  }
+
+  return chunks
+}
+
+// https://usehooks.com/useWindowSize/
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  })
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    // Add event listener
+    window.addEventListener('resize', handleResize)
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize()
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize)
+  }, []) // Empty array ensures that effect is only run on mount
+
+  return windowSize
+}
