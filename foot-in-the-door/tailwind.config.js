e359c4e60a4c9fca85701c01e5b7cf6b6ab09793
Kjell <kjell@leanside.com>
2020-10-22 16:05:03 -0500

    fix tailwind config: extend height, don't reset
    Also enable formatting

--- foot-in-the-door/tailwind.config.js
+++ foot-in-the-door/tailwind.config.js
@@ -1,21 +1,25 @@
+/** @format */
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
   purge: {
-    content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+    content: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+    ],
   },
   theme: {
     extend: {
       colors: {
         black: '#232323',
       },
+      height: {
+        'screen-3/4': '75%',
+        'screen-4/5': '80%',
+      },
     },
-    height: {
-     'screen-3/4': '75%',
-     'screen-4/5': '80%',
-    }
   },
   variants: {
     display: ['responsive', 'hover', 'focus', 'group-hover', 'group-focus'],
