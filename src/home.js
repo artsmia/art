1536b7053ff9b83e06bb1053cb1543e1cf0b53d4
Teran Keith <terankeith33@gmail.com>
2022-05-26 22:17:16 -0500

    testing deployment to prod with simple change. Will revert

--- src/home.js
+++ src/home.js
@@ -83,7 +83,7 @@ var HomeDepartmentsAndPages = React.createClass({
       </div>
       <div className="welcome mdl-grid">
       <div className="mdl-cell mdl-cell--9-col">
-        <p>It is our mission to enrich the community by collecting, preserving, and making accessible outstanding works of art from the world’s diverse cultures. With over 90,000 artworks, Mia’s collection includes art from six continents, spanning about 5,000 years.  Here you will find world-famous artworks that embody the highest levels of artistic achievement and speak to the enduring power of human creativity to shape our world. </p>
+        <p>IT is our mission to enrich the community by collecting, preserving, and making accessible outstanding works of art from the world’s diverse cultures. With over 90,000 artworks, Mia’s collection includes art from six continents, spanning about 5,000 years.  Here you will find world-famous artworks that embody the highest levels of artistic achievement and speak to the enduring power of human creativity to shape our world. </p>
       </div>
       </div>
       <Departments compact={true} />
