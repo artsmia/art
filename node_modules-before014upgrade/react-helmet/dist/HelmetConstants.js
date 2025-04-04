8dff46c79fd6ab73f0e25076450b6d0e022f7c72
Kjell <kjell@leanside.com>
2025-04-04 16:15:42 +0000

    Commit untracked changes made on the live server

---
+++ node_modules-before014upgrade/react-helmet/dist/HelmetConstants.js
@@ -0,0 +1,18 @@
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var TAG_NAMES = {
+    META: "meta",
+    LINK: "link"
+};
+
+exports.TAG_NAMES = TAG_NAMES;
+var TAG_PROPERTIES = {
+    NAME: "name",
+    CHARSET: "charset",
+    HTTPEQUIV: "http-equiv",
+    REL: "rel",
+    HREF: "href",
+    PROPERTY: "property"
+};
+exports.TAG_PROPERTIES = TAG_PROPERTIES;
---
+++ node_modules-before014upgrade/react-helmet/dist/index.js
@@ -0,0 +1,26 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+require("core-js/fn/array/for-each");
+
+require("core-js/fn/array/from");
+
+require("core-js/fn/map");
+
+require("core-js/fn/object/get-own-property-symbols");
+
+require("core-js/fn/object/is");
+
+require("core-js/fn/object/keys");
+
+require("core-js/fn/set");
+
+var _Helmet = require("./Helmet");
+
+var _Helmet2 = _interopRequireDefault(_Helmet);
+
+exports["default"] = _Helmet2["default"];
+module.exports = exports["default"];
