5b2e9e9b22c6e1b42c7b52771200fa9209997097
MAI <mibrahim@artsmia.org>
2026-02-25 15:43:00 -0600

    Updated styling for 404 error page to match info site

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,19 +1,58 @@
 var React = require("react");
+var { Link } = require("react-router");
 var { isInternetArchivedPage } = require("../util/archived-page-handler.js");
+var Footer = require("../footer");
 
-var coverPageStyle = {
-  textAlign: "center",
-  background: "#232323",
-  minHeight: "100vh",
-};
+var WORDMARK_SRC =
+  "https://mia-styleguide.s3.amazonaws.com/dist/images/mia-wordmark.svg";
+var WORDMARK_FALLBACK = "/images/MIA_LOGO_WORDMARK.svg";
+var ERROR_IMG =
+  "https://s3.amazonaws.com/mia-images/not-found.jpg";
 
 var NotFound = React.createClass({
+  componentDidMount() {
+    if (typeof document !== "undefined" && document.body) {
+      document.body.classList.add("not-found-page");
+    }
+  },
+
+  componentWillUnmount() {
+    if (typeof document !== "undefined" && document.body) {
+      document.body.classList.remove("not-found-page");
+    }
+  },
+
   render() {
     return (
-      <div style={coverPageStyle}>
-        <h2 style={{ color: "white", fontSize: "333%", paddingTop: "17%" }}>
-          404 Not Found
-        </h2>
+      <div className="not-found-page">
+        <div className="container">
+          <div className="not-found-wordmark">
+            <Link to="home" aria-label="Minneapolis Institute of Art">
+              <img
+                src={WORDMARK_SRC}
+                alt="Minneapolis Institute of Art"
+                onError={(e) => {
+                  if (e.target.src !== WORDMARK_FALLBACK) {
+                    e.target.src = WORDMARK_FALLBACK;
+                  }
+                }}
+              />
+            </Link>
+          </div>
+          <main>
+            <img
+              src={ERROR_IMG}
+              alt='A black and white photo of several women standing beside a chalkboard twice and a handmade sign which read "Lost Persons Dept"'
+            />
+            <h1>404. Sorry! Couldn't find that page :(</h1>
+            <p>
+              We have recently redesigned our site and things may have moved. If
+              your link didn't work or you are not finding what you are looking
+              for please use the search.
+            </p>
+          </main>
+          <Footer />
+        </div>
       </div>
     );
   },
